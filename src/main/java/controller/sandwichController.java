818c2f955a2535739447a43e1472b4587a11339d
Lovelyse Lete Zitongo <lovelysedu94@hotmail.fr>
2020-01-29 15:55:59 +0100

    edit ok

--- src/main/java/controller/sandwichController.java
+++ src/main/java/controller/sandwichController.java
@@ -2,32 +2,32 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import model.TypeProduit;
 import repositories.ProduitRepository;
 
 @Controller
+@RequestMapping("/page")
 public class sandwichController {
 
 	@Autowired
 	ProduitRepository produitRepository;
+
+	@GetMapping("/listSandwich")
+	public String listAdmin(Model model) {
+		model.addAttribute("produit", produitRepository.findAllByType(TypeProduit.Sandwich));
+		return "page/listSandwich";
+	}	
 	
-	@GetMapping("/page/listSandwich")
-	public ModelAndView list() {
-		return new ModelAndView("/page/listSandwich", "sandwichs", produitRepository.findAllByType(TypeProduit.Sandwich));
+	@GetMapping("/listSandwich/delete")
+	public String delete(@RequestParam(name="id") Long id) {
+		produitRepository.deleteById(id);
+		return "redirect:/page/listSandwich";
 	}
 
-	
-	@GetMapping("/page/listSandwichClient")
-	public ModelAndView listSandwich() {
-		return new ModelAndView("/page/listSandwichClient", "sandwichs", produitRepository.findAll()); 
-	}
-	
-	
-//	public ModelAndView add() {
-//		
-//	}
 }
